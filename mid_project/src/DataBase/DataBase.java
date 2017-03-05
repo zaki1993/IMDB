30648d3c6c90d67dc33420a012d626ac3c113028
Ivan Todorov <Ivan Todorov>
2017-03-05 21:05:27 +0200

    Fix DataBase

--- mid_project/src/DataBase/DataBase.java
+++ mid_project/src/DataBase/DataBase.java
@@ -1,18 +1,17 @@
 package DataBase;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.Map.Entry;
 
 public class DataBase implements IDataBase{
 	
-	private HashMap<Object, LinkedHashMap<String, Object>> database;
+	private HashSet<Object> database;
 	private String[] fields;
 	private String type;
 	
 	public DataBase(String type, String[] fields){
-		this.database = new HashMap<>();
+		this.database = new HashSet();
 		this.fields = new String[fields.length];
 		int size = 0;
 		for(String i : fields){
@@ -26,37 +25,19 @@ public class DataBase implements IDataBase{
 	}
 	
 	public boolean insert(Object object){
-		//TODO explain how this works
-		//TODO fix it 
-		/*
-		if(!object.getClass().getName().equals(type)){
-			System.out.println("Invalid entry data!");
-			return;
+		if (object == null) {
+			return false;
 		}
-		*/
-		//if the object is contained and is not the same then change the properties
-		if(database.containsKey(object)){
-			//database.remove(object);
-		}
-		String data = object.toString();
-		String[] splitData = data.substring(1, data.length() - 1).split(", ");
-		
-		LinkedHashMap<String, Object> temp = new LinkedHashMap<>();
-		for (int i = 0; i < fields.length; i++) {
-			temp.put(fields[i], splitData[i]);
-		}
-		database.put(object, temp);
-//		System.out.println("Successfully inserted!");
+		this.database.add(object);
 		return true;
 	}
 	
 	public void print(){
 		System.out.println("Table: " + getType());
-		for(Entry<Object, LinkedHashMap<String, Object>> i : database.entrySet()){
-			for(Entry<String, Object> j : i.getValue().entrySet()){
-				System.out.println("\t" + j.getKey() + ": " + j.getValue());
-			}
-			System.out.println("");
+		System.out.println();
+		for (Object object : database) {
+			System.out.println(object);
+			System.out.println();
 		}
 	}
 	
--- mid_project/src/controller/Demo.java
+++ mid_project/src/controller/Demo.java
@@ -26,35 +26,35 @@ public class Demo {
 		Movie movie1 = new Movie("Pesho kalibrata", "Poster za pesho kalibrata",genre, actiorche, null, "Mnogo qk film", LocalDate.of(2005, Month.AUGUST, 12));
 //		System.out.println(movie1);
 		Movie movie2 = new Movie("Pesho kalibrata2", "Poster za pesho kalibrata2",genre, null, null, "Mnogo po qk film prosto", LocalDate.of(2005, Month.AUGUST, 12));
-//		x.insert(movie1);
-//		x.insert(movie2);
-//		x.print();
-//		
+		x.insert(movie1);
+		x.insert(movie2);
+		x.print();
+		
 		User pesho = User.register("Pesho", (byte) 27, "Bulgaria");
-//		User gosho = User.register("Gosho", (byte) 50, "Polsha");
-//		users.insert(pesho);
-//		users.insert(gosho);
-//		
-//		pesho.vote(movie1, 5);
-//		pesho.vote(movie1, 4);
-//		gosho.vote(movie1, 9);
-//		System.out.println("-------------------After vote----------------------");
-//		//update database
-//		//works fine
-//		x.insert(movie1);
-//		x.print();
-//		System.out.println("-----------------------------------------");
+		User gosho = User.register("Gosho", (byte) 50, "Polsha");
+		users.insert(pesho);
+		users.insert(gosho);
+		
+		pesho.vote(movie1, 5);
+		pesho.vote(movie1, 4);
+		gosho.vote(movie1, 9);
+		System.out.println("-------------------After vote----------------------");
+		//update database
+		//works fine
+		x.insert(movie1);
+		x.print();
+		System.out.println("-----------------------------------------");
 		pesho.addToWatchList(movie2);
-//		pesho.addToWatchList(movie2);
+		pesho.addToWatchList(movie2);
 		pesho.addToWatchList(movie1);
-		System.out.println(pesho);
-//		gosho.addToWatchList(movie1);
-//		users.insert(pesho);
-//		users.insert(gosho);
-//		users.print();
-//		System.out.println("-----------------------------------------");
-//		System.out.println(pesho.toString());
-//		System.out.println("-----------------------------------------");
-//		System.out.println(movie1.toString());
+//		System.out.println(pesho);
+		gosho.addToWatchList(movie1);
+		users.insert(pesho);
+		users.insert(gosho);
+		users.print();
+		System.out.println("-----------------------------------------");
+		System.out.println(pesho.toString());
+		System.out.println("-----------------------------------------");
+		System.out.println(movie1.toString());
 	}	
 }
